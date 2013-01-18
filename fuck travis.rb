d9de317401556164f16871c245cb45bbd15aa8bc
Patrick Neff <pneff@anpisolutions.com>
2013-01-18 15:53:57 -0600

    --Initial Commit--

---
+++ fuck travis.rb
@@ -0,0 +1,9 @@
+#Because fuck Travis.
+require 'nest_thermostat'
+@nest = NestThermostat::Nest.new email: ENV['NEST_EMAIL'], password: ENV['NEST_PASSWORD']
+loop do
+  if @nest.temperature <= 70 and (Time.now.hour >= 8 and Time.now.hour <= 17)
+    @nest.temperature = 71
+  end
+  sleep 10
+end
