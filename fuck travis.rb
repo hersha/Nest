e667f16221e95e04b2e43b2ab379ee4b525bbdeb
Patrick Neff <pneff@anpisolutions.com>
2013-01-25 09:19:15 -0600

    Anti-Lindig fix

--- fuck travis.rb
+++ fuck travis.rb
@@ -4,6 +4,8 @@ require 'nest_thermostat'
 loop do
   if @nest.temperature <= 70 and (Time.now.hour >= 8 and Time.now.hour <= 17)
     @nest.temperature = 71
+  elsif @nest.temperature >= 72 and Time.now.hour == 8
+    @nest.temperature = 71
   end
   sleep 10
 end
