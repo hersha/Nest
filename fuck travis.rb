2900ea71722384f040cce4d9c586ccccf861d862
Patrick Neff <pneff@anpisolutions.com>
2013-02-01 11:35:57 -0600

    Acceptable range update

--- fuck travis.rb
+++ fuck travis.rb
@@ -2,7 +2,7 @@
 require 'nest_thermostat'
 @nest = NestThermostat::Nest.new email: ENV['NEST_EMAIL'], password: ENV['NEST_PASSWORD']
 loop do
-  if @nest.temperature <= 70 and (Time.now.hour >= 8 and Time.now.hour <= 17)
+  if (@nest.temperature <= 70 or @nest.temperature >= 75) and (Time.now.hour >= 8 and Time.now.hour <= 17)
     @nest.temperature = 71
   #LINDIIIIIIIIIIIIG!!!!!
   elsif @nest.temperature >= 72 and Time.now.hour == 8
